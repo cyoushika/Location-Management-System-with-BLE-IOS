35a005d7f1b1ed26f8a83028451eeeb2c93acd2d
cyoushika <Tears@mm-dhcp-129-195.naist.jp>
2017-03-31 11:34:22 +0900

    Change the SignUp Page from ViewControl to TableControl
    I changed the ViewControl to TableControl since it has Static Mode and
    it is more suitable for the SignUp page. And I also use the Navigation
    Controller to do the “Back to HomePage” job instead of a button. It
    makes the page clearly. Since the TableView has Scroll Function, so I
    don’t need put the table on a Scroll Sheet.

--- Location Manage System with BLE/Location Manage System with BLE/SignUpViewController.swift
+++ Location Manage System with BLE/Location Manage System with BLE/SignUpViewController.swift
@@ -1,15 +1,38 @@
 import UIKit
 
-class SignUpViewController : UIViewController,UIPickerViewDataSource,UIPickerViewDelegate
+class SignUpTableViewController : UITableViewController,UIPickerViewDataSource,UIPickerViewDelegate
 {
     
+    var flag = 1
     var pickerData: [String] = [String]()
+    
+    
+    @IBOutlet weak var PhotoImage: UIImageView!
+    @IBOutlet weak var PhotoCell: UITableViewCell!
+    @IBOutlet weak var NameTextField: UITextField!
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var SexLabel: UILabel!
+    @IBOutlet weak var SexCell: UITableViewCell!
+    @IBOutlet weak var PickerCell: UITableViewCell!
+    @IBOutlet weak var Picker: UIPickerView!
+    
+    @IBAction func Accept(_ sender: UIButton) {
+        // When user tap the Accept Button, upload all the information
+        print("Accept")
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerData = ["Male","Female"]
-        //self.picker.dataSource = self
-        //self.picker.delegate = self
+        Picker.delegate = self
+        Picker.dataSource = self
+        PhotoImage.image = UIImage(named: "photo-icon.png")
+        NameTextField.placeholder = "Please input your name"
+        EmailTextField.placeholder = "Please input your Email"
+        // Only when User is trying edit SexCell, the PickerView will show up
+        PickerCell.isHidden = true
+        SexLabel.text = "Male/Female"
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,11 +51,32 @@ class SignUpViewController : UIViewController,UIPickerViewDataSource,UIPickerVie
         return pickerData[row]
     }
     
-    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-        return 150
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return 30
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.section == 0
+        {
+            //When user tap the PhotoCell, Take or upload a picture
+            print("Photo")
+            PhotoCell.isSelected = false
+        }
+        
+        if indexPath.section == 3
+        {
+            SexCell.isSelected = false
+            
+            if flag == 1
+            {
+                PickerCell.isHidden = false
+                SexLabel.textColor = UIColor.red
+                flag = 2
+            }
+            else if flag == 2
+            {
+                SexLabel.text = pickerData[Picker.selectedRow(inComponent: 0)]
+                SexLabel.textColor = UIColor.black
+                PickerCell.isHidden = true
+                flag = 1
+            }
+        }
     }
 }
