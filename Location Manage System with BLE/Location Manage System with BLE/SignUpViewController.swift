573e4607b05565ed430102b19e83c9e9c98253d3
cyoushika <Tears@mm-dhcp-129-195.naist.jp>
2017-03-31 16:15:30 +0900

    Changed the Sex Selection Cell from PickerView to Segement
    Made the selection more directly and more comfortable.

--- Location Manage System with BLE/Location Manage System with BLE/SignUpViewController.swift
+++ Location Manage System with BLE/Location Manage System with BLE/SignUpViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class SignUpTableViewController : UITableViewController,UIPickerViewDataSource,UIPickerViewDelegate
+class SignUpTableViewController : UITableViewController
 {
     
     var flag = 1
@@ -11,44 +11,36 @@ class SignUpTableViewController : UITableViewController,UIPickerViewDataSource,U
     @IBOutlet weak var PhotoCell: UITableViewCell!
     @IBOutlet weak var NameTextField: UITextField!
     @IBOutlet weak var EmailTextField: UITextField!
-    @IBOutlet weak var SexLabel: UILabel!
-    @IBOutlet weak var SexCell: UITableViewCell!
-    @IBOutlet weak var PickerCell: UITableViewCell!
-    @IBOutlet weak var Picker: UIPickerView!
+    
+    @IBOutlet weak var SexSegement: UISegmentedControl!
     
     @IBAction func Accept(_ sender: UIButton) {
         // When user tap the Accept Button, upload all the information
-        print("Accept")
+        if (NameTextField.text != nil && EmailTextField.text != nil && SexSegement.titleForSegment(at: SexSegement.selectedSegmentIndex) != nil)
+        {
+            print(NameTextField.text)
+            print(EmailTextField.text)
+            print(SexSegement.titleForSegment(at: SexSegement.selectedSegmentIndex))
+            print("Accept!")
+        }
+        else {
+            print("Error!")
+        }
+        
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerData = ["Male","Female"]
-        Picker.delegate = self
-        Picker.dataSource = self
         PhotoImage.image = UIImage(named: "photo-icon.png")
         NameTextField.placeholder = "Please input your name"
         EmailTextField.placeholder = "Please input your Email"
         // Only when User is trying edit SexCell, the PickerView will show up
-        PickerCell.isHidden = true
-        SexLabel.text = "Male/Female"
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row]
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -60,23 +52,5 @@ class SignUpTableViewController : UITableViewController,UIPickerViewDataSource,U
             PhotoCell.isSelected = false
         }
         
-        if indexPath.section == 3
-        {
-            SexCell.isSelected = false
-            
-            if flag == 1
-            {
-                PickerCell.isHidden = false
-                SexLabel.textColor = UIColor.red
-                flag = 2
-            }
-            else if flag == 2
-            {
-                SexLabel.text = pickerData[Picker.selectedRow(inComponent: 0)]
-                SexLabel.textColor = UIColor.black
-                PickerCell.isHidden = true
-                flag = 1
-            }
-        }
     }
 }
